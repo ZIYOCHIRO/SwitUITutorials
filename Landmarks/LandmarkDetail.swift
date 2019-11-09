9005ad96aa57bf9d94a18ba671b3a2ff7a35deb6
ZIYOCHIRO <ziyochiro@gmail.com>
2019-11-09 11:51:42 +0800

    first commit

---
+++ Landmarks/LandmarkDetail.swift
@@ -0,0 +1,46 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by ruixinyi on 2019/11/8.
+//  Copyright © 2019 ruixinyi. All rights reserved.
+//
+
+import SwiftUI
+
+struct LandmarkDetail: View {
+    // describes the view's contents and layout
+    var landmark: Landmark
+    var body: some View {
+        VStack {
+            MapView(coordinate: landmark.locationCoordinate)
+                .edgesIgnoringSafeArea(.top)
+                .frame(height: 300)
+            
+            CircleImage(image: landmark.image)
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            
+            VStack(alignment: .leading) {
+                Text(landmark.name)
+                    .font(.title)
+                HStack {
+                    Text(landmark.park)
+                        .font(.subheadline)
+                    Spacer()
+                    Text(landmark.state)
+                        .font(.subheadline)
+                }
+            }.padding()
+            
+            Spacer()
+        }
+        .navigationBarTitle(Text(verbatim: landmark.name), displayMode: .inline)
+    }
+}
+
+struct LandmarkDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkDetail(landmark: landmarkData[0])
+    }
+}
